59eb0cc3ea914471a4a90aeab93c4e64b416e545
jimartinezkuhn <124900928+jimartinezkuhn@users.noreply.github.com>
2023-02-09 11:42:55 -0300

    Add files via upload

---
+++ mostrar_ocultos.js
@@ -0,0 +1,36 @@
+document.getElementById("link_linkedin").onmouseover = function() {
+    console.log("iupi");
+    document.getElementById("linkedin").style.display = 'block'; 
+}
+
+document.getElementById("link_linkedin").onmouseout = function() {
+    document.getElementById("linkedin").style.display = 'none'; 
+}
+
+document.getElementById("link_tel").onmouseover = function() {
+    console.log("iupi");
+    document.getElementById("tel").style.display = 'block'; 
+}
+
+document.getElementById("link_tel").onmouseout = function() {
+    document.getElementById("tel").style.display = 'none'; 
+}
+
+document.getElementById("link_mail").onmouseover = function() {
+    console.log("iupi");
+    document.getElementById("mail").style.display = 'block'; 
+}
+
+document.getElementById("link_mail").onmouseout = function() {
+    document.getElementById("mail").style.display = 'none'; 
+}
+
+document.getElementById("link_github").onmouseover = function() {
+    console.log("iupi");
+    document.getElementById("github").style.display = 'block'; 
+}
+
+document.getElementById("link_github").onmouseout = function() {
+    document.getElementById("github").style.display = 'none'; 
+}
+
